1d7bdbb4ac0b7aca57c74bff5801d4cd345351e3
Nescitus <grzegorz.starzec@student.wat.edu.pl>
2024-01-10 16:41:54 +0100

    Dodanie instruktazu - cdn

--- src/app/instruktaz/instruktaz.component.ts
+++ src/app/instruktaz/instruktaz.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+export interface Instruktaz {
+  dzien: string;
+  godzina: string;
+  nazwa: string;
+  uwagi: string;
+}
 
 @Component({
   selector: 'app-instruktaz',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './instruktaz.component.html',
-  styleUrl: './instruktaz.component.css'
+  styleUrls: ['./instruktaz.component.css']
 })
 export class InstruktazComponent {
+  instruktaze: Instruktaz[] = [
+    {
+      dzien: '01-01-2022',
+      godzina: '17:00',
+      nazwa: 'Instruktaż na kompanię',
+      uwagi: 'brak',
+    },
+    {
+      dzien: 'a',
+      godzina: 'a',
+      nazwa: 'a',
+      uwagi: 'a',
+    }];
 
+  addInstruktaz(newInstruktaz: Instruktaz) {
+    this.instruktaze.push(newInstruktaz);
+  }
 }
