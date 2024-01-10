61330148542f9756987686a557ad6b907475e463
Nescitus <grzegorz.starzec@student.wat.edu.pl>
2024-01-10 15:28:41 +0100

    Dodanie instruktazu

---
+++ src/app/instruktaz/instruktaz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instruktaz',
+  standalone: true,
+  imports: [],
+  templateUrl: './instruktaz.component.html',
+  styleUrl: './instruktaz.component.css'
+})
+export class InstruktazComponent {
+
+}
