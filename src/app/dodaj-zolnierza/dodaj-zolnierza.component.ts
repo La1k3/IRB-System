63a3dd0eea7b3b8e411bc61f281c64e1ca09c898
Łukasz Tokarz <lukasz.tokarz01@student.wat.edu.pl>
2024-01-10 20:00:45 +0100

    Dodaj żołnierza done

--- src/app/dodaj-zolnierza/dodaj-zolnierza.component.ts
+++ src/app/dodaj-zolnierza/dodaj-zolnierza.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface Zolnierz {
+  ranga: string;
+  imie: string;
+  nazwisko: string;
+  numer_sluzbowy: string;
+  numer_legitymacji: string;
+  stanowisko: string;
+}
+
 
 @Component({
   selector: 'app-dodaj-zolnierza',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './dodaj-zolnierza.component.html',
   styleUrl: './dodaj-zolnierza.component.css'
 })
 export class DodajZolnierzaComponent {
 
+  onSubmit(newZolnierz : Zolnierz) {
+    console.log(newZolnierz)
+  }
 }
